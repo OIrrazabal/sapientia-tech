184edd8fd358342af5dfcc2b717137b20f97107c
mauricioomar <baezmauri177@gmail.com>
2025-04-13 20:45:24 -0300

    Bcrypt instalar

--- db/inserts.sql
+++ db/inserts.sql
@@ -1,6 +1,7 @@
 -- Insertar usuarios de prueba
 INSERT INTO usuarios (nombre, email, contraseña, es_admin, telefono, direccion) VALUES
 ('Admin1', 'admin@example.com', 'admin123', 1, '0981-123456', 'Calle 1'),
-('Usuario1', 'user1@example.com', 'user1', 0, '0982-654321', 'Calle 2'),
-('Usuario2', 'user2@example.com', 'user2', 0, '0983-987654', 'Calle 3'),
-('Usuario3', 'user3@example.com', 'user3', 0, '0984-654987', 'Calle 4');
+('Usuario1', 'user1@example.com', '$2b$10$DK.c8.zD/6J3p/YaICptUO5ISIMd.MhPbm52AGjb1kWPJ.Qa1gREW', 0, '0982-654321', 'Calle 2'),
+('Usuario2', 'user2@example.com', '$2b$10$DK.c8.zD/6J3p/YaICptUO5ISIMd.MhPbm52AGjb1kWPJ.Qa1gREW', 0, '0983-987654', 'Calle 3'),
+('Usuario3', 'user3@example.com', '$2b$10$DK.c8.zD/6J3p/YaICptUO5ISIMd.MhPbm52AGjb1kWPJ.Qa1gREW', 0, '0984-654987', 'Calle 4');
+
