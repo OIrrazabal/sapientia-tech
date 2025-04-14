e455ecfd3c8a8a0360977de546a2c60380b687bb
mauricioomar <baezmauri177@gmail.com>
2025-04-14 20:25:06 -0300

    Crear servicios en modelo

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -11,71 +11,35 @@ router.get('/login', (req, res) => {
 });
 
 
-router.post('/login/try', (req, res) => {
+router.post('/login/try', async (req, res) => {
     // Obtener datos del formulario
     const { email, password } = req.body;
 
     if (!email?.trim() || !password?.trim()) {
-        return res.status(400).send('contraseña o email vacio');
+        return res.status(400).json({ error: 'Campos vacíos' });
     }
 
     if (password.length < 6) {
-        return res.status(400).send('contraseña muy corta');
+        return res.status(400).json({ error: 'Contraseña muy corta' });
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        return res.status(400).send('Email invalido');
+        return res.status(400).json({ error: 'Email invalido' });
     }
 
-    // Buscar usuario en BD
-    const query = 'SELECT * FROM usuarios WHERE email = ? AND contraseña = ?';
-    db.get(query, [email, password], (err, user) => {
-        if (!user) {
-            return res.redirect('/public/login');
-        }
+    const user = await Usuario.loginAdmin(username, password);
+        
+    if (!user) {
+        return res.redirect('/public/admin-login');
+    }
 
-        if (user.es_admin === 1) {
-            return res.redirect('/public/login');
-        }
-
-        // Usuario válido y no admin
-        res.redirect('/auth/home');
-    });
+    // Validar si es admin usando el servicio
+    if (!Usuario.isAdmin(user)) {
+        return res.redirect('/public/admin-login');
+    }
+    // Usuario válido y admin
+    res.redirect('/auth/home');
 });
-
-router.post('/admin-login/try', (req, res) => {
-    // Obtener datos del formulario
-    const { email, password } = req.body;
-
-    if (!email?.trim() || !password?.trim()) {
-        return res.status(400).send('contraseña o email vacio');
-    }
-
-    if (password.length < 6) {
-        return res.status(400).send('contraseña muy corta');
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        return res.status(400).send('Email invalido');
-    }
-
-    // Buscar usuario en BD
-    const query = 'SELECT * FROM usuarios WHERE email = ? AND contraseña = ?';
-    db.get(query, [email, password], (err, user) => {
-        if (!user) {
-            return res.redirect('/public/login');
-        }
-
-        if (user.es_admin !== 1) {
-            return res.redirect('/public/login');
-        }
-
-        // Usuario válido y no admin
-        res.redirect('/auth/home');
-    });
-});
-
 
 module.exports = router;
