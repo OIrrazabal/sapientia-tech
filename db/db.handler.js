de4a662816ee252e8360a712cae616eb8f5482cb
mauricioomar <baezmauri177@gmail.com>
2025-06-08 09:24:42 -0300

    Log Admin

--- db/db.handler.js
+++ db/db.handler.js
@@ -4,11 +4,8 @@ const dbHandler = {
     ejecutarQuery: (query, params = []) => {
         return new Promise((resolve, reject) => {
             db.get(query, params, (err, row) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(row);
-                }
+                if (err) reject(err);
+                else resolve(row);
             });
         });
     },
@@ -16,11 +13,18 @@ const dbHandler = {
     ejecutarQueryAll: (query, params = []) => {
         return new Promise((resolve, reject) => {
             db.all(query, params, (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows);
-                }
+                if (err) reject(err);
+                else resolve(rows);
+            });
+        });
+    },
+
+    // ✅ ESTA ES LA NUEVA FUNCIÓN DENTRO DEL OBJETO
+    ejecutarQueryConResultado: (query, params = []) => {
+        return new Promise((resolve, reject) => {
+            db.run(query, params, function (err) {
+                if (err) reject(err);
+                else resolve(this); // this contiene lastID
             });
         });
     }
